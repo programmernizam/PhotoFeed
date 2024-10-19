b7b97f22ce29093edcce4141dcf65734c0e5ca71
programmerNizam <mdnizam12109@gmail.com>
2024-10-19 20:30:12 +0600

    Navbar created

---
+++ components/Logo.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <h2 className="font-extrabold text-2xl">PHOTOFEED</h2>
+    </Link>
+  );
+}
